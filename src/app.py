dc49c5694869e602a77dc8290accba7f57c35ce3
Jacqueline Silva <silva.jacqueline2718@gmail.com>
2026-05-16 16:36:52 -0300

    commit 6: adiciona rotas de livros e autores

--- src/app.py
+++ src/app.py
@@ -9,7 +9,32 @@ def home():
 @app.route("/status")
 def status():
     return {"status": "API online"}
-``
+
+
+@app.route("/")
+def home():
+    return "Sistema de Biblioteca"
+
+@app.route("/sobre")
+def sobre():
+    return "Projeto desenvolvido na disciplina de Integração e Entrega Contínua"
+
+@app.route("/livros")
+def livros():
+    return "Lista de livros cadastrados"
+
+
+@app.route("/autores")
+def autores():
+    return "Lista de autores cadastrados"
+
+@app.route("/contato")
+def contato():
+    return "Página de contato"
+
+
+
 
 if __name__ == "__main__":
     app.run(debug=True)
+
