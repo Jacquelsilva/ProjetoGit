68dade87b551e371937af8cf5a32c32ffe6d29a6
Jacqueline Silva <silva.jacqueline2718@gmail.com>
2026-05-17 01:00:42 -0300

    test: adiciona teste da rota sobre

--- tests/test_app.py
+++ tests/test_app.py
@@ -8,3 +8,8 @@ def test_home():
     client = app.test_client()
     response = client.get("/")
     assert response.status_code == 200
+
+def test_sobre():
+    client = app.test_client()
+    response = client.get("/sobre")
+    assert response.status_code == 200
